3202947fea0ed30577ea3601287d820616c9f938
md-tahmid-hasan-golap <golapraj47@gmail.com>
2026-05-28 17:22:34 +0600

    add a parcels

--- src/components/MyParcel.jsx
+++ src/components/MyParcel.jsx
@@ -1,7 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { AuthContext } from "../Auth/AuthContext";
+import UseAxiusCecure from "./UseAxiusCecure";
 const MyParcel = () => {
+  const { user } = useContext(AuthContext);
+  const axiosSecure = UseAxiusCecure();
+  const { data: parcels = [] } = useQuery({
+    queryKey: ["parcels", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/myparcels/${user?.email}`);
+      return res.data;
+    },
+  });
   return (
     <div>
-      <h2>This is my parcel</h2>
+      <h2>This is my parcel {parcels.length}</h2>
     </div>
   );
 };
